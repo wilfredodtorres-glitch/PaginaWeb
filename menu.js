e1e912a395b71b78584467cdb96ed16810d232e0
wilfredodtorres-glitch <wilfredo.d.torres@gmail.com>
2026-05-26 19:37:42 -0600

    Summary
    cambios en la tercera clse

--- menu.js
+++ menu.js
@@ -5,7 +5,7 @@ document.getElementById('menu').innerHTML = `
         <svg xmlns="http://www.w3.org/2000/svg" height="20" width="30" viewBox="0 0 512 512"><!--!Font Awesome Free v7.2.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2026 Fonticons, Inc.-->
             <path d="M277.8 8.6c-12.3-11.4-31.3-11.4-43.5 0l-224 208c-9.6 9-12.8 22.9-8 35.1S18.8 272 32 272l16 0 0 176c0 35.3 28.7 64 64 64l288 0c35.3 0 64-28.7 64-64l0-176 16 0c13.2 0 25-8.1 29.8-20.3s1.6-26.2-8-35.1l-224-208zM240 320l32 0c26.5 0 48 21.5 48 48l0 96-128 0 0-96c0-26.5 21.5-48 48-48z">
         </svg>Menú Principal Principal</a>
-      <a href="SobreMi.html">
+      <a href="sobreMi.html">
         <svg xmlns="http://www.w3.org/2000/svg" height="20" width="30" viewBox="0 0 512 512">
           <path d="M399 384.2C376.9 345.8 335.4 320 288 320l-64 0c-47.4 0-88.9 25.8-111 64.2 35.2 39.2 86.2 63.8 143 63.8s107.8-24.7 143-63.8zM0 256a256 256 0 1 1 512 0 256 256 0 1 1 -512 0zm256 16a72 72 0 1 0 0-144 72 72 0 1 0 0 144z"/>
         </svg>Sobre mí
